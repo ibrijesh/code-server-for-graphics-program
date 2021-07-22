e2a90765c5e593b9c1d6d48eb55fc52764a6156f
Brijesh <brijeshyadavjthj@gmail.com>
2021-07-22 13:49:17 +0530

    dynamic port for heroku

--- conf.d/nginx-conf.sh
+++ conf.d/nginx-conf.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-nginx -g 'daemon off;'
 
-#sed -i -e 's/$PORT/'"$PORT"'/g' /etc/nginx/sites-enabled/default && nginx -g 'daemon off;'
+sed -i -e 's/$PORT/'"$PORT"'/g' /etc/nginx/sites-enabled/default && nginx -g 'daemon off;'
