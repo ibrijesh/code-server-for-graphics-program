b8d9c393959dc63acd9733ab35338c545d3b65c3
Brijesh <brijeshyadavjthj@gmail.com>
2021-07-22 12:35:11 +0530

    dynamic port variable removed

--- conf.d/nginx-conf.sh
+++ conf.d/nginx-conf.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+nginx -g 'daemon off;'
 
-sed -i -e 's/$PORT/'"$PORT"'/g' /etc/nginx/sites-enabled/default && nginx -g 'daemon off;'
+#sed -i -e 's/$PORT/'"$PORT"'/g' /etc/nginx/sites-enabled/default && nginx -g 'daemon off;'
